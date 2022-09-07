42428b3d24e2e47594b9a8591916690f3b6e44c0
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-07 20:24:30 +0430

    crete new features

---
+++ Bnpp.Web/Controllers/ManagementDocumentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class ManagementDocumentsController : Controller
+    {
+
+        [Route("ManagementDocuments")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Methodology()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult OtherDocuments()
+        {
+            return View();
+        }
+
+         
+
+        public IActionResult ManagementReviews()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bnpp.Web/Controllers/TechnicalController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class TechnicalController : Controller
+    {
+        [Route("Technical")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GeneralData()
+        {
+            return View();
+        }
+        
+
+        public IActionResult DesignData()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult Documents()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult Safety()
+        {
+            return View();
+        }
+
+        public IActionResult Components()
+        {
+            return View();
+        }
+        
+    }
+}
