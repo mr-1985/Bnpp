fb9cd60feeabd940a4821f3f817983245b9c6efd
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-08 22:53:09 +0430

    installation

---
+++ Bnpp.Web/Controllers/InstallationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class InstallationController : Controller
+    {
+        [Route("Installation")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
