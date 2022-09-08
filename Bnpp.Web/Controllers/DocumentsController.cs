355a21899108a1cc52257c2c338d60a3f2279956
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-08 18:04:31 +0430

    Documents Of Technical

---
+++ Bnpp.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        [Route("Documents")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OperationalDocuments()
+        {
+            return View();
+        }
+
+        public IActionResult Drawing()
+        {
+            return View();
+        }
+
+        public IActionResult Standards()
+        {
+            return View();
+        }
+
+        public IActionResult ManufacturerDocuments()
+        {
+            return View();
+        }
+
+        public IActionResult InstallationDocuments()
+        {
+            return View();
+        }
+
+        public IActionResult MaintenanceDocuments()
+        {
+            return View();
+        }
+
+        public IActionResult AgeingDocuments()
+        {
+            return View();
+        }
+    }
+}
