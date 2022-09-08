84b12cb21723e85c30a9a2a1fd01c3d167ee904c
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-09 00:18:02 +0430

    operational

---
+++ Bnpp.Web/Controllers/OperationalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class OperationalController : Controller
+    {
+        [Route("Operational")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sensors()
+        {
+            return View();
+        }
+
+        public IActionResult ChemicalWater()
+        {
+            return View();
+        }
+
+        public IActionResult Environmental()
+        {
+            return View();
+        }
+
+
+    }
+}
