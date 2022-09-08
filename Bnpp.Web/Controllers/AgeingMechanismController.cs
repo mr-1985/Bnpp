92cd932c12ff94db061d58fd605899da2fa4eec8
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-08 22:37:50 +0430

    aeging mechanism

---
+++ Bnpp.Web/Controllers/AgeingMechanismController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class AgeingMechanismController : Controller
+    {
+        [Route("AgeingMechanism")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Mechanism()
+        {
+            return View();
+        }
+
+        public IActionResult Documents()
+        {
+            return View();
+        }
+    }
+}
