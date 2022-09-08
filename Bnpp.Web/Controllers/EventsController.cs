922a5c2fb2eec388fec39970cb6633f838b667f9
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-09 02:17:52 +0430

    Maintenance

---
+++ Bnpp.Web/Controllers/EventsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        [Route("Events")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bnpp.Web/Controllers/MaintenanceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        [Route("Maintenance")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MaintenancePrograms()
+        {
+            return View();
+        }
+
+        public IActionResult Defections()
+        {
+            return View();
+        }
+
+        public IActionResult MaintenanceForms()
+        {
+            return View();
+        }
+
+        public IActionResult SpareParts()
+        {
+            return View();
+        }
+
+        public IActionResult Measurements()
+        {
+            return View();
+        }
+
+        public IActionResult DefectsReport()
+        {
+            return View();
+        }
+    }
+}
