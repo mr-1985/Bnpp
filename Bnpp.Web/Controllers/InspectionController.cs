9efb9e98da03840b235c247271edb0ef1cae1dee
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-09 16:49:49 +0430

    inspection

---
+++ Bnpp.Web/Controllers/InspectionController.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class InspectionController : Controller
+    {
+        [Route("Inspection")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SubMenu()
+        {
+            return View();
+        }
+
+        public IActionResult InspectionReports()
+        {
+            return View();
+        }
+
+        public IActionResult InspectionInstructions()
+        {
+            return View();
+        }
+
+        public IActionResult InspectionPrograms()
+        {
+            return View();
+        }
+
+
+        #region SubMenu
+
+        public IActionResult TypicalPrograms()
+        {
+            return View();
+        }
+
+        public IActionResult WorkingPrograms()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
