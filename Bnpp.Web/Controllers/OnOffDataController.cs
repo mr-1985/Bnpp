1fa008df53cfba4bacf8abfb06b65812f8019b7e
Majid Roueintan <m.roueintan518@gmail.com>
2022-09-09 09:53:19 +0430

    On Off Data

---
+++ Bnpp.Web/Controllers/OnOffDataController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bnpp.Web.Controllers
+{
+    public class OnOffDataController : Controller
+    {
+        [Route("OnOffData")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
